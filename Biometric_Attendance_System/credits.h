73eb7e710fe3ffd9177412216ef81634c06f903d
Vishal Umavane <vsumavane@outlook.com>
2024-12-15 16:58:32 +0530

    Update credits.h

--- Biometric_Attendance_System/credits.h
+++ Biometric_Attendance_System/credits.h
@@ -1,11 +1,11 @@
 #ifndef CREDITS_H
 #define CREDITS_H
 
-#define DEFAULT_WIFI_SSID "UmavaneLAN"  // default WiFi ssid
-#define DEFAULT_WIFI_PASS "19112002"  //default WiFi password
+#define DEFAULT_WIFI_SSID "aRedmi"  // default WiFi ssid
+#define DEFAULT_WIFI_PASS "aaaaaaaa"  //default WiFi password
 #define DEFAULT_mdns "biometric"           //default WiFi mdns adress , you can acess the web portal at http://biometric.local
-#define DEFAULT_displayname "ULHASNAGR LLC" //default Organization name which will be displayed on the OLED
-#define DEFAULT_wwwusername "golu"        //default Login id
-#define DEFAULT_wwwpassword "golu123"        //default Login password
+#define DEFAULT_displayname "ULHASNAGAR LLC" //default Organization name which will be displayed on the OLED
+#define DEFAULT_wwwusername "iti"        //default Login id
+#define DEFAULT_wwwpassword "iti@123"        //default Login password
 
 #endif
