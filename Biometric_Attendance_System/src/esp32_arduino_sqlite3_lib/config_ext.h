8676cbd7e25ff2343ffa42b230e2bcfc11274e4e
Vishal Umavane <vsumavane@outlook.com>
2025-05-11 03:35:17 +0530

    update sqlite3 with latest release

--- Biometric_Attendance_System/src/esp32_arduino_sqlite3_lib/config_ext.h
+++ Biometric_Attendance_System/src/esp32_arduino_sqlite3_lib/config_ext.h
@@ -10,7 +10,6 @@
 #define SQLITE_DEFAULT_LOOKASIDE        512,64
 #define YYSTACKDEPTH                        20
 #define SQLITE_SMALL_STACK                   1
-#define SQLITE_DEFAULT_PAGE_SIZE          4096
 #define SQLITE_SORTER_PMASZ                  4
 #define SQLITE_DEFAULT_CACHE_SIZE           -1
 #define SQLITE_DEFAULT_MEMSTATUS             0
@@ -23,13 +22,14 @@
 #define SQLITE_DISABLE_FTS3_UNICODE          1
 #define SQLITE_DISABLE_FTS4_DEFERRED         1
 #define SQLITE_LIKE_DOESNT_MATCH_BLOBS       1
-#define SQLITE_DEFAULT_FOREIGN_KEYS          0
+#define SQLITE_DEFAULT_FOREIGN_KEYS          1
 #define SQLITE_DEFAULT_LOCKING_MODE          1
-#define SQLITE_DEFAULT_PAGE_SIZE          4096
+#define SQLITE_DEFAULT_PAGE_SIZE           512
 #define SQLITE_DEFAULT_PCACHE_INITSZ         8
 #define SQLITE_MAX_DEFAULT_PAGE_SIZE     32768
 #define SQLITE_POWERSAFE_OVERWRITE           1
 #define SQLITE_MAX_EXPR_DEPTH                0
+#undef SQLITE_OMIT_FOREIGN_KEY
 
 /*
 #undef SQLITE_OMIT_ALTERTABLE
