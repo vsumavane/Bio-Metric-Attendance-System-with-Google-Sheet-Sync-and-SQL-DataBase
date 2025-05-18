80d07b370bd3818d461297729954085ba257c71a
Vsumavane <vsumavane@outlook.com>
2025-05-18 16:57:07 +0530

    refactor: remove vite-plugin-compression and clean up configuration

--- Admin Panel/vite.config.js
+++ Admin Panel/vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
-  plugins: [tailwindcss(), viteCompression({
-      algorithm: 'gzip',
-      deleteOriginFile: true,
-    })],
-    publicDir:'public',
+  plugins: [tailwindcss(),]
 });
