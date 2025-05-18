41525b0a565b28b67482fbfb0e074988033d45cf
Vsumavane <vsumavane@outlook.com>
2025-05-18 16:42:22 +0530

    refactor: remove unused vite-plugin-singlefile dependency and clean up configuration

--- Admin Panel/vite.config.js
+++ Admin Panel/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-import {viteSingleFile} from "vite-plugin-singlefile";
 import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
-  plugins: [tailwindcss(), viteSingleFile(), viteCompression({
+  plugins: [tailwindcss(), viteCompression({
       algorithm: 'gzip',
       deleteOriginFile: true,
     })],
